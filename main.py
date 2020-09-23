08119551bb8e604075e5cb5829f1ecc637b6a398
Saad Khan <sk6786@g.rit.edu>
2020-09-22 23:00:07 -0400

    initial

---
+++ main.py
@@ -0,0 +1,78 @@
+from multiprocessing import Process, Queue
+import time
+import random
+class RangeReceiver:
+    def __init__(self, checkingInterval, expireTime):
+        self.checkingInterval = checkingInterval
+        self.expireTime = expireTime
+        self.lastUpdated = time.time()
+        self.checkingTime = time.time()
+    def receive_mpg(self, q):
+        while True:
+            msg = q.get()
+            print('received message')
+            if 'mpg' in msg:
+                self.update_time()
+                print('new time ' + str(self.lastUpdated))
+                q.put({'lastUpdated': self.lastUpdated})
+    def update_time(self):
+        self.lastUpdated = time.time()
+    def check_alive(self, q):
+        while True:
+            msg = q.get(timeout = 10)
+            if 'lastUpdated' in msg:
+                self.lastUpdated = msg['lastUpdated']
+                print('updating last updated')
+            time.sleep(self.checkingInterval)
+            self.checkingTime = time.time()
+            print('current time ' + str(self.checkingTime) + 'last Updated' + str(self.lastUpdated) + 'expire time' + str(self.expireTime))
+            # print('type ' + str(type(self.checkingTime)))
+            difference = self.checkingTime - self.lastUpdated
+            print('the difference is ' + str(difference))
+            if difference > self.expireTime:
+                # print('calling fault monitor')
+                FaultMonitor.failure_detected()
+
+class RangeSender:
+    def __init__(self, interval, range, gasAmount):
+        self.sendinterval = interval
+        self.gasAmount = gasAmount
+        self.range = range
+    def heartbeat(self, q):
+        while True:
+            # print('Im sleeping')
+            time.sleep(self.sendinterval)
+            print('getting mpg')
+            q.put({'mpg':self.current_mpg()})
+    def current_mpg(self):
+        self.gasAmount = self.gasAmount - random.randrange(1,4)
+        self.range = self.range - random.randrange(1,20)
+        print(self.gasAmount)
+        if self.gasAmount < 0:
+            self.gasAmount = 0
+        mpg = self.range / self.gasAmount
+        print('mpg is '+ str(mpg))
+        return mpg
+
+
+class FaultMonitor:
+    @staticmethod
+    def failure_detected():
+        print('The core process has failed')
+
+
+
+if __name__ == '__main__':
+    q = Queue()
+    range_receiver = RangeReceiver(5, 20)
+    range_sender = RangeSender(5, 200, 15)
+    fault_monitor = FaultMonitor()
+    p = Process(target= range_sender.heartbeat, args = (q,))
+    p.start()
+    p2 = Process(target=range_receiver.check_alive, args = (q,))
+    p2.start()
+    p3 = Process(target=range_receiver.receive_mpg, args=(q,))
+    p3.start()
+    p.join()
+    p2.join()
+    p3.join()
